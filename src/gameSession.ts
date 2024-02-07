eb81f1be315656862e7afedf0f85c2ad0c86c61e
Jelle Wolbers <jelle.wolbers@tno.nl>
2024-02-07 21:02:18 +0100

    Converted codebase to use TypeScript (#5). Installed and configured Prettier (#6)

--- src/gameSession.ts
+++ src/gameSession.ts
@@ -1,33 +1,37 @@
+import { Data } from "ws"
 import { log } from "./utils.js"
+import { Player, WebSocketWithId } from "./types.js"
 
 export default class GameSession {
+    players: Player[]
+
     constructor() {
         this.players = []
     }
 
-    addPlayer(socket) {
-        const newPlayer = {
+    addPlayer(socket: WebSocketWithId) {
+        const newPlayer: Player = {
             state: null,
             socket: socket,
         }
         this.players.push(newPlayer)
     }
 
-    processMessage(unparsedMessage, socket) {
-        const message = JSON.parse(unparsedMessage)
-        switch (message.method) {
-            case 'player_state':
+    processMessage(unparsedMessage: Data, socket: WebSocketWithId) {
+        const message = JSON.parse(unparsedMessage.toString())
+        switch (message.type) {
+            case "PlayerStateMessage":
                 const player = this.players.find((player) => player.socket.id === socket.id)
                 player.state = message.data
                 break
             default:
-                console.warn(`Unknown message method: ${message.method}`)
+                console.warn(`Unknown message type: ${message.type}`)
                 break
         }
     }
 
     removePlayer(socketId) {
         log(`Socket ${socketId} closed, removing corresponding player`)
-        this.players = this.players.filter(player => player.socket.id !== socketId)
+        this.players = this.players.filter((player) => player.socket.id !== socketId)
     }
 }
---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export function log(message: string) {
+    console.log(`${getCurrentTime()} ${message}`)
+}
+
+function getCurrentTime() {
+    const date = new Date()
+    const time = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`
+    return time
+}
